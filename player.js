e2dfbd8c2f365ae2cf737d4f82315e666ba100f0
2154takeru-netizen <2154takeru@gmail.com>
2026-02-09 00:17:22 +0900

    Add files via upload

--- player.js
+++ player.js
@@ -1,13 +1,30 @@
-const player = {
-  name: "武内 健流",
-  pos: "三塁手",
-  avg: ".312",
-  hr: 5,
-  rbi: 18
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+const players = {
+  1: {
+    name: "武内 健流",
+    pos: "投手",
+    avg: ".312",
+    hr: 5,
+    rbi: 18
+  },
+  2: {
+    name: "三浦 憲大",
+    pos: "外野手",
+    avg: ".280",
+    hr: 2,
+    rbi: 10
+  }
 };
 
-document.getElementById("player-name").textContent = player.name;
-document.getElementById("player-pos").textContent = player.pos;
-document.getElementById("avg").textContent = player.avg;
-document.getElementById("hr").textContent = player.hr;
-document.getElementById("rbi").textContent = player.rbi;
+if (!players[id]) {
+  document.body.innerHTML = "選手データがありません";
+} else {
+  const p = players[id];
+  document.getElementById("player-name").textContent = p.name;
+  document.getElementById("player-pos").textContent = p.pos;
+  document.getElementById("avg").textContent = p.avg;
+  document.getElementById("hr").textContent = p.hr;
+  document.getElementById("rbi").textContent = p.rbi;
+}
